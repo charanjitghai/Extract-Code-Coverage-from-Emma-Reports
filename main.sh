0b870715d5eeb31125cb67028e93332623fc9b40
charanjitghai <charanjitghai@github.com>
2016-10-04 00:53:06 -0700

    removed ambiguity

--- main.sh
+++ main.sh
@@ -2,9 +2,9 @@ pwd=`pwd`
 rm allProjFiles.out
 rm allZeroCoverageFiles.out
 rm allProjZeroCoverageFiles.out
-cd $AVR/fusionapps/crm/components/crmCommon/zcx/content/publicModelBase/src
+cd $projDirectory
 find . -name '*.java' | grep -Eo '[A-Za-z]*.java' > $pwd/allProjFiles.out
-cd $MW_HOME/jdeveloper/ide/extensions/oracle.apps.common.ide.emmaextension.2/CrmCommon.jws/coverage/_files
+cd $emmaFilesDirectory
 grep -rl 'COVERAGE SUMMARY FOR PACKAGE' * | xargs grep -Eo '[A-Za-z]*.java</A></TD><TD CLASS="h">0%' | grep -Eo '[A-Za-z]*.java' > $pwd/allZeroCoverageFiles.out
 sort $pwd/allProjFiles.out -o $pwd/allProjFiles.out
 sort $pwd/allZeroCoverageFiles.out -o $pwd/allZeroCoverageFiles.out
